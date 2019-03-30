c4343dca448b185efe93d6a107d2e67df11d6e41
YA <YA-androidapp@users.noreply.github.com>
2019-03-30 11:54:26 +0900

    Update 01_facetrim.py

--- 01_facetrim.py
+++ 01_facetrim.py
@@ -22,34 +22,33 @@ datadir = 'C:/opencv-4.0.1-vc14_vc15'
 
 # リサイズ後のサイズ
 image_size = 100
+# 顔検出時の最小サイズ
+min_face_size = 100
 
 # 分類器のパス一覧
 haarcascades = [
-    #'sources/data/haarcascades/haarcascade_frontalcatface.xml',
-    #'sources/data/haarcascades/haarcascade_frontalcatface_extended.xml',
-    #'sources/data/haarcascades/haarcascade_frontalface_alt.xml',
-    #'sources/data/haarcascades/haarcascade_frontalface_alt_tree.xml',
-    #'sources/data/haarcascades/haarcascade_frontalface_alt2.xml',
-    #'sources/data/haarcascades/haarcascade_frontalface_default.xml',
-    #'sources/data/haarcascades/haarcascade_profileface.xml',
+    # 'sources/data/haarcascades/haarcascade_frontalcatface.xml',
+    # 'sources/data/haarcascades/haarcascade_frontalcatface_extended.xml',
+    # 'sources/data/haarcascades/haarcascade_frontalface_alt.xml',
+    # 'sources/data/haarcascades/haarcascade_frontalface_alt_tree.xml',
+    # 'sources/data/haarcascades/haarcascade_frontalface_alt2.xml',
+    # 'sources/data/haarcascades/haarcascade_frontalface_default.xml',
+    # 'sources/data/haarcascades/haarcascade_profileface.xml',
 
-    #'sources/data/haarcascades_cuda/haarcascade_frontalface_alt.xml',
-    #'sources/data/haarcascades_cuda/haarcascade_frontalface_alt_tree.xml',
-    #'sources/data/haarcascades_cuda/haarcascade_frontalface_alt2.xml',
-    #'sources/data/haarcascades_cuda/haarcascade_frontalface_default.xml',
-    #'sources/data/haarcascades_cuda/haarcascade_profileface.xml',
-    'lbpcascade_animeface.xml', # https://github.com/nagadomi/lbpcascade_animeface
-    ]
+    # 'sources/data/haarcascades_cuda/haarcascade_frontalface_alt.xml',
+    # 'sources/data/haarcascades_cuda/haarcascade_frontalface_alt_tree.xml',
+    # 'sources/data/haarcascades_cuda/haarcascade_frontalface_alt2.xml',
+    # 'sources/data/haarcascades_cuda/haarcascade_frontalface_default.xml',
+    # 'sources/data/haarcascades_cuda/haarcascade_profileface.xml',
+    'lbpcascade_animeface.xml',  # https://github.com/nagadomi/lbpcascade_animeface
+]
 
-count = 1
 
-def image_data(file, haarcascade):
-    global count
+def image_data(file, count_originalfile, haarcascade):
     cascade_file = os.path.join(datadir.replace('/', os.path.sep), haarcascade)
-    print('{} {}'.format(file, cascade_file))
 
     image = cv2.imread(file)
-    image_gs = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # グレースケール画像を取得
+    image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # グレースケール画像を取得
     # =============================================================================
     #     scaleFactor 各画像スケールにおける縮小量を表します
     #     minNeighbors 物体候補となる矩形は，最低でもこの数だけの近傍矩形を含む必要があります
@@ -58,35 +57,38 @@ def image_data(file, haarcascade):
     # =============================================================================
     cascade = cv2.CascadeClassifier(cascade_file)
     # 顔領域を取得
-    face_list = cascade.detectMultiScale(image_gs,scaleFactor=1.1,minNeighbors=1,minSize=(100,100))
-    if len(face_list)>0:
-        for i,face in enumerate(face_list):
-            x,y,w,h = face
-            face = image[y:y+h,x:x+w]
-            face = cv2.resize(face,(image_size,image_size))
-            newdir = str((os.path.sep).join(file.split(os.path.sep)[0:-1]))+'_face'
+    face_list = cascade.detectMultiScale(
+        image_gs, scaleFactor=1.1, minNeighbors=1, minSize=(min_face_size, min_face_size))
+    if len(face_list) > 0:
+        count_face = 0
+        for i, face in enumerate(face_list):
+            x, y, w, h = face
+            face = image[y:y+h, x:x+w]
+            face = cv2.resize(face, (image_size, image_size))
+            newdir = str((os.path.sep).join(
+                file.split(os.path.sep)[0:-1]))+'_face'
             if not os.path.exists(newdir):
                 os.mkdir(newdir)
-            cv2.imwrite(os.path.join(newdir,str(count)+'.png'),face)
-            count += 1
-            print('    {} {}'.format(file, count))
+            output_file = os.path.join(newdir, str(count_originalfile)+'_'+str(count_face)+'.png')
+            cv2.imwrite(output_file, face)
+            count_face += 1
+            print('         {:.2%} {}'.format((count_face/len(face_list)), output_file))
     else:
         print('no face')
 
+
 def main():
-    global count
-    subdirs = glob(os.path.join(scrpath,root_dirname, '**'))
+    subdirs = glob(os.path.join(scrpath, root_dirname, '**'))
     for subdir in subdirs:
-        count = 1
         print('sub directory: {}'.format(subdir))
         files = glob(os.path.join(subdir, '*.jpg'))
-        c=0
+        count_originalfile = 0
         for file in files:
             for haarcascade in haarcascades:
                 time.sleep(0.1)
-                print('  {:.2%} {}'.format((c/len(files)), file))
-                image_data(file, haarcascade.replace('/', os.path.sep))
-                c+=1
+                print('  {:.2%} {}'.format((count_originalfile/len(files)), file))
+                image_data(file, count_originalfile, haarcascade.replace('/', os.path.sep))
+                count_originalfile += 1
 
 
 if __name__ == '__main__':
