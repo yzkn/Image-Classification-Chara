eb9eeb3dcfbc1e599acc1ba38617805fcf7736a5
YA <YA-androidapp@users.noreply.github.com>
2019-03-31 11:53:43 +0900

    Update 03_holdout.py

--- 03_holdout.py
+++ 03_holdout.py
@@ -2,6 +2,8 @@
 
 from glob import glob
 import os
+import random
+import shutil
 
 scrpath = os.path.abspath(os.path.dirname(__file__))
 os.chdir(scrpath)
@@ -11,22 +13,34 @@ os.chdir(scrpath)
 root_dirname = 'data'
 
 # 訓練データと検証データに分割したデータセットをこのディレクトリに出力
-root_dirname = 'dataset'
+output_dirname = 'dataset'
+
+output_train_dirname = 'train'
+output_validate_dirname = 'validate'
 
 
 def main():
     subdirs = glob(os.path.join(scrpath, root_dirname, '*_face_expand'))
-    # for subdir in subdirs:
-    #     print('sub directory: {}'.format(subdir))
-    #     files = glob(os.path.join(subdir, '*.png'))
-    #     count_originalfile = 0
-    #     for file in files:
-    #         print('  {:.2%} {}'.format((count_originalfile/len(files)), file))
-    #         newdir = str((os.path.sep).join(
-    #             file.split(os.path.sep)[0:-1]))+'_expand'
-    #         if not os.path.exists(newdir):
-    #             os.mkdir(newdir)
-    #         count_originalfile += 1
+    for subdir in subdirs:
+        print('sub directory: {}'.format(subdir))
+        newdirs = [
+            os.path.join(scrpath, output_dirname, output_train_dirname, os.path.basename(subdir)),
+            os.path.join(scrpath, output_dirname, output_validate_dirname, os.path.basename(subdir))
+        ]
+        for newdir in newdirs:
+            if not os.path.exists(newdir):
+                os.makedirs(newdir, exist_ok=True)
+
+        files = glob(os.path.join(subdir, '*.png'))
+        count_originalfile = 0
+        for file in files:
+            print('  {:.2%} {} {}'.format((count_originalfile/len(files)), subdir, file))
+            if os.path.isfile(file):
+                if random.random() <= 0.8:
+                    shutil.copyfile(file, os.path.join(newdirs[0], os.path.basename(file)))
+                else:
+                    shutil.copyfile(file, os.path.join(newdirs[1], os.path.basename(file)))
+            count_originalfile += 1
 
 
 if __name__ == '__main__':
